be3254dc6cd0ca8afdd9904d515d22c65050dddb
brahim-slimani <slimani.ibr@gmail.com>
2022-03-20 15:00:46 +0100

    Refactor base64 utils

--- dist/jwt-decoder.js
+++ dist/jwt-decoder.js
@@ -5,35 +5,52 @@ function InvalidTokenException(message) {
 InvalidTokenException.prototype = new Error();
 InvalidTokenException.prototype.name = "InvalidTokenException";
 
-function bas64_decode(str) {
-    return Buffer.from(str, "base64");
+/**
+ * Decode Base64 to UTF-8 string
+ * @param {String} str encoded base64 string
+ * @returns Decoded string in UTF-8
+ */
+const bas64_decode = (str) => {
+    return decodeURIComponent(atob(str));
 }
 
-payload = (token) => {
+/**
+ * Resolve payload data from JWT token
+ * @param {String} token to be decoded
+ * @returns Data claims
+ */
+const payload = (token) => {
     try {
         const splitedToken = token.replace("Bearer ", "").split(".");
         const decodedClaims = JSON.parse(bas64_decode(splitedToken[1]));
         return decodedClaims;
     } catch (error) {
-        throw InvalidTokenException("Invalid token error: "+error.message);
+        throw InvalidTokenException("Invalid token error: " + error.message);
     }
 }
 
-header = (token) => {
+/**
+ * Resolve header from token
+ * @param {String} token to be encoded
+ * @returns JWT Header
+ */
+const header = (token) => {
     try {
         const splitedToken = token.replace("Bearer ", "").split(".");
         const decodedHeader = JSON.parse(bas64_decode(splitedToken[0]));
         return decodedHeader;
     } catch (error) {
-        throw InvalidTokenException("Invalid token error: "+error.message);
+        throw InvalidTokenException("Invalid token error: " + error.message);
     }
 }
 
-resolve = (token) => {
-    return {
-        header: header(token),
-        payload: payload(token)
-    }
+/**
+ * Resolve payload data and header from JWT token
+ * @param {String} token to be decoded
+ * @returns entire jwt data claims
+ */
+const resolve = (token) => {
+    return { header: header(token), payload: payload(token) }
 }
 
 module.exports = {
